07c60c7af1141eafb7b694107d5e8a6a0becb75a
SATYA1962S <naraypxh.com>
2022-12-10 16:27:39 +0530

    First Commit

---
+++ block.js
@@ -0,0 +1,70 @@
+
+
+ const SHA256 = require('crypto-js/sha256');
+ const hex2ascii = require('hex2ascii');
+ 
+ 
+ class Block {
+ 
+     
+     constructor(data){
+         this.hash = null;                                           
+         this.height = null;
+         this.body =Buffer.from(JSON.stringify(data)).toString('hex') ;
+         this.time = 0;
+         this.previousBlockHash = null;
+     }                                                 
+
+         
+
+        
+         
+         
+         
+                                                           
+             
+                                      
+             
+     
+           validate() {
+               let self = this;
+               return new Promise((resolve, reject) => {
+                try {
+                const currentHash = self.hash;
+                self.hash = null;
+                const newHash = SHA256(JSON.stringify(self)).toString();
+                self.hash = currentHash;
+                resolve(currentHash  === newHash);
+                } catch ( error) {
+                reject(new Error(err));
+                }
+         
+        });
+     }
+
+     getBData() { 
+        let self = this;
+        return new Promise( async ( resolve,reject) => {
+            let enc_data = this.body;
+            let dec_data = hex2ascii(enc_data);
+            let decdata_in_JSON = JSON.parse(dec_data);
+            if ( this.height ==0) {
+                resolve("This is the Genesis block ");
+
+            } else {
+                resolve(decdata_in_JSON);
+            }
+
+        });
+     }
+     
+     
+
+         
+
+ 
+     
+ 
+ }
+ 
+ module.exports.Block = Block; 
---
+++ blockchain.js
@@ -0,0 +1,182 @@
+
+
+
+ const SHA256 = require('crypto-js/sha256');
+ const BlockClass = require('./block.js');
+ const bitcoinMessage = require('bitcoinjs-message');
+
+ 
+ class Blockchain {
+ 
+     constructor() {
+         this.chain = [];
+         this.height = -1;
+         this.initializeChain();
+     }
+ 
+    
+     async initializeChain() {
+         if( this.height === -1){
+             let block = new BlockClass.Block({data: 'Genesis Block'});
+             await this._addBlock(block);
+         }
+     }
+ 
+    
+     getChainHeight() {
+         return new Promise((resolve, reject) => {
+             resolve(this.height);
+         });
+     }
+
+    
+     _addBlock(block) {
+         let self = this;
+         return new Promise(async (resolve, reject) => {
+            block.height = self.chain.length;
+            block.time = new Date().getTime().toString().slice(0,-3);
+            if(self.chain.length > 0){
+                block.previousBlockHash = self.chain[self.chain.length-1].hash;
+            }
+            block.hash = SHA256(JSON.stringify(block)).toString();
+            console.debug('validation of chain starts');
+            let errors = await self.validateChain();
+            console.log(errors);
+            console.debug('validation of Chain ends');
+            if ( errors.length === 0){
+                self.chain.push(block);
+                self.height++ ;
+                resolve(block)
+            }else{
+                reject(errors);
+            }
+        });
+    
+
+            
+         
+     }
+ 
+     requestMessageOwnershipVerification(address) {
+         return new Promise((resolve) => {
+            const OwnershipMssage  = `${address}:new Date().getTime().toString().slice(0,-3):starRegistry`;
+                   resolve(OwnershipMessage  );
+
+             
+         });
+    }
+     submitStar(address, message, signature, star) {
+         let self = this;
+         return new Promise(async (resolve, reject) => {
+            let temps = parseInt(message.split(':')[1]);
+            let currentTime = parseInt(new Date().getTime().toString().slice(0,-3));
+            if((currentTime - temps) <  ( 5 * 60)) {
+                if(bitcoinMessage.verify(messge,address,signature)){
+                    let block = new BlockClass.Block({"owner": address,"star":star});
+                    self._addBlock(block);
+                    resolve(block);
+                } else {
+                    reject ( Error('Message is not verified'))
+                }
+
+            } else {
+                reject(Error('too much time has passed, stay below 5 minutes'))
+            }    
+
+                
+            
+             
+         });
+     }
+ 
+     
+     getBlockByHash(hash) {
+         let self = this;
+         return new Promise((resolve, reject) => {
+            const block = self.chain.filter(block => block.hash ===hash);
+            if ( typeof block !=  'undefined'){
+                resolve(block);
+            
+            }else{
+                reject(Error("No block with this hash"))
+            }
+            
+         });
+     }
+ 
+     
+    
+     getBlockByHeight(height) {
+         let self = this;
+         return new Promise((resolve, reject) => {
+             let block = self.chain.filter(p => p.height === height)[0];
+             if(block){
+                 resolve(block);
+             } else {
+                 resolve(null);
+             }
+         });
+     }
+ 
+     
+     getStarsByWalletAddress (address) {
+         let self = this;
+         let stars = [];
+         return new Promise((resolve, reject) => {
+            self.chain.forEach(async(b) =>{
+            let data = await b.getBData();
+            if(data){
+                if(data.owner === address){
+                    stars.push(data);
+
+                }
+
+            }
+
+         })
+         resolve (stars);
+
+
+             
+        });
+     }
+ 
+     
+     validateChain() {
+         let self = this;
+         let errorLog = [];
+         return new Promise( (resolve) => {
+            let validatePromises = [];
+            self.chain.forEach((block,index) => {
+                if (block.height  > 0){
+                    const previousBlock = self.chain[index-1];
+                    if (block.previousBlockHash !== previousBlock.hash){
+                        const errorMessage  =  `Block ${index}previousBlockHash set  to ${ block.previousBlockHash},but actual previous block was {previousBlock.hash}`;
+                        errorLog.push(errorMessage);
+                    
+                    }
+
+                }
+            validatePromises.push(block.validate());
+
+             
+         
+        });
+
+        Promise.all(validatePromises)
+             .then(validatedBlocks  =>  {
+                validatedBlocks.forEach((valid,index) => {
+                    if(!valid){
+                        const invalidBlock = self.chain[index];
+                        const  errorMessage = `Block ${index}hash (${invalidBlock.hash})is invalid`;
+                        errorLog.push(errorMessge);
+                    }
+                    
+                });
+                resolve(errorLog);
+             });
+         });
+    }
+}
+
+module.exports.Blockchain = Blockchain;   
